fd0467618731b3764613c06f06eab187b20c9b73
richard <richard@owain.wood.rox>
2016-02-04 23:24:17 -0500

    Shuffled things around a bit. midiseq has gone. config.h now used to configure muxes etc

--- src/mcuio.cpp
+++ src/mcuio.cpp
@@ -1,3 +1,60 @@
+#include "Arduino.h"
+#include "mcuio.h"
+#include "config.h"
+
+#ifdef DEBUG_MODE
+#include "debug.h"
+#endif
+
+#include "interrupts.h"
+
+// the setup routine runs once when you press reset:
+void setup() 
+{
+  #ifdef DEBUG_MODE
+  Serial.begin(9600);
+  #endif
+
+  // set pin directions 
+  init_pins();
+
+  // user will need to sweep pots etc
+  //calibrate_analog_ins();
+
+  // platform-specific interrupt init
+  #ifdef ARDUINO_AVR_UNO 
+    init_interrupts_uno();
+  #elif ENERGIA 
+    init_interrupts_energia();
+  #endif
+}
+
+/* 2KHz */
+void isr_0()
+{
+  scan_mux(&encoder_mux);
+  //process_encoder_data(encoder_mux,tweakr,4,0);
+  //read_analog_in(0);
+}
+
+/* 1Hz */
+void isr_1()
+{
+  log_debug("mx1vl1",encoder_mux.value[0]);
+  log_debug("mx1vl2",encoder_mux.value[1]);
+  //log_debug("ana0val",analog_ins[0].value);
+  
+  #ifdef DEBUG_MODE
+  dump_debugs();
+  #endif
+  //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void loop()
+{
+
+}
+
 void init_pins()
 {
   unsigned int i;
--- src/mcuio.h
+++ src/mcuio.h
@@ -1,7 +1,6 @@
 #ifndef MCUIO_H
 #define MCUIO_H
 
-
 #define NUM_DIGITAL_INS (sizeof digital_ins / sizeof digital_ins[0])
 #define NUM_DIGITAL_OUTS (sizeof digital_outs / sizeof digital_outs[0])
 #define NUM_ANALOG_INS (sizeof analog_ins / sizeof analog_ins[0])
@@ -58,4 +57,5 @@ void calibrate_analog_ins();
 void scan_mux(digital_mux *mux);
 void read_analog_in(int);
 void process_encoder_data(struct digital_mux mux, struct rotary_encoder *encoder, char num_pairs, char set_index);
+
 #endif /* MCUIO_H */
